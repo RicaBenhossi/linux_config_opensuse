0cc2d33c936f771c34033ef9db0de2447d365c75
Luis Ricardo Benhossi <ricardo.benhossi@gmail.com>
2024-04-12 09:47:29 -0300

    Restore installation of packman.

--- run_to_configure_pc.zsh
+++ run_to_configure_pc.zsh
@@ -42,7 +42,7 @@ echo
 rm ~/.config/gtk-4.0/gtk.css &&
 ln $root_my_config/configs/gtk-X.0-gtk.css ~/.config/gtk-4.0/gtk.css &&
 rm ~/.config/gtk-3.0/gtk.css &&
-ln $root_my_config/configs/gtk-3.0-gtk.css ~/.config/gtk-4.0/gtk.css
+ln $root_my_config/configs/gtk-X.0-gtk.css ~/.config/gtk-3.0/gtk.css
 echo
 echo "====================> Downloading .gitconfig file"
 echo
@@ -92,14 +92,34 @@ echo 'Press any key to continue...'; read -k1 -s
 echo
 sudo zypper install -t pattern devel_python3
 echo
-# Install this if youtube videos won´t run
-# echo "====================> Install Packman fix"
-# echo
-# echo 'Press any key to continue...'; read -k1 -s
-# echo
-# sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials &&
-# sudo zypper dup --from packman-essentials --allow-vendor-change
-# echo
+echo "--------------------> libopenssl-devel"
+echo
+echo 'Press any key to continue...'; read -k1 -s
+echo
+sudo zypper -n install openssl libopenssl-devel
+echo
+echo "--------------------> lzma-devel"
+echo
+echo 'Press any key to continue...'; read -k1 -s
+echo
+sudo zypper -n install openssl lzma-devel
+echo
+echo "====================> Install Packman fix"
+echo
+echo 'Press any key to continue...'; read -k1 -s
+echo
+sudo zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials &&
+sudo zypper dup --from packman-essentials --allow-vendor-change
+echo
+echo "====================> Install VSCoce Insiders"
+echo
+echo 'Press any key to continue...'; read -k1 -s
+echo
+sudo zypper ar https://packages.microsoft.com/yumrepos/vscode vscode &&
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+sudo zypper refresh &&
+sudo zypper install code code-insiders
+echo
 echo
 read -rsp $"Configuration process finished. Press any key to proceed..."
 echo
